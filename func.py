c5d87fbcb629e1e56fa0f88a4b3ac5a36298d652
Frédéric KLEIN <74149300+fklein82@users.noreply.github.com>
2023-11-10 17:21:14 +0100

    save

--- func.py
+++ func.py
@@ -66,5 +66,8 @@ def main():
             plt.axis('off')
             plt.title(f"Prédiction: {prediction[1]} (Confiance: {prediction[2]*100:.2f}%)")
             plt.show()
+            return f"Prédiction: {prediction[1]} (Confiance: {prediction[2]*100:.2f}%)"
+
         else:
             print("L'image n'a pas pu être téléchargée.")
+
